9154f80a812e1169ac2737b1180a4dca21605b9b
Misheck Ithagu <misheck@flexcom.co.ke>
2022-08-03 11:23:47 +0300

    Added Security Measures

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(helmet());
   //swagger configuration
   const config = new DocumentBuilder()
     .setTitle('Commision Claims App Api')
